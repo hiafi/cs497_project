bc6c99895f0e37c57b75203dee2853d4af64a705
Jonathan <hiafi3@gmail.com>
2013-05-02 21:50:34 -0700

    Forgot to add files

--- Log.py
+++ Log.py
@@ -8,55 +8,95 @@
 
 #------------------------------------------------------------------------------
 class Log:
-    def __init__(self):
-        self.duration        = 0.0       #Length (in seconds) of connection
-        self.protocol        = ""        #Protocol name (tcp, udp)
-        self.service         = ""        #Service on destination (http, telnet)
-        self.flag            = ""        #normal or error status
-        self.src_bytes       = 0         #number of bytes from source to destination
-        self.dst_bytes       = 0         #number of bytes from destination to source
-        self.land            = False     #True if connection is to / from the same host / port
-        self.wrong_format    = 0         #number of "wrong" fragments
-        self.urgent          = 0         #number of urgent packets
-        self.hot             = 0         #number of "hot" indicators
-        self.failed_logins   = 0         #number of failed login attempts
-        self.logged_in       = False     #true if successfully loged in
-        self.compromised     = 0         #number of "compromised" conditions
-        self.root_shell      = False     #True if root shell was obtained
-        self.su_attempted    = False     #True if "su root" was attempted
-        self.roots           = 0         #number of "root" accesses
-        self.files_created   = 0         #number of file creation operations
-        self.shells          = 0         #number of shell prompts
-        self.access_files    = 0         #number of operations on access control files
-        self.outbound_cmds   = 0         #number of outbound commands in an ftp session
-        self.is_host_login   = False     #True if the login belongs to the host list
-        self.is_guest_login  = False     #True if the login is a guest login
-        self.conn_count      = 0         #number of connections to the same host in the last 2 seconds
-    
-        #The following features refer to the same host connections
-        self.srv_count           = 0     # number of connections to the same service
-                                         #as the current connection in the past 2 secs
-        self.s_error_rate        = 0.0   # % of connections that have SYN errors
-        self.srv_s_error_rate    = 0.0   # % of connections that have SYN errors
-        self.r_error_rate        = 0.0   # % of connections that have REJ errors
-        self.srv_r_error_rate    = 0.0   # % of connections that have REJ errors
-        self.same_srv_rate       = 0.0   # % of the connections to the same service
-        self.diff_srv_rate       = 0.0   # % of connections to different services
-    
-        #The following features refer to the same service connections
-        self.srv_diff_host_rate          = 0.0   # % of connections to different hosts
-        self.dst_host_count              = 0
-        self.dst_host_srv_count          = 0
-        self.dst_host_same_srv_rate      = 0.0
-        self.dst_host_diff_srv_rate      = 0.0
-        self.dst_host_same_src_port_rate = 0.0
-        self.dst_host_srv_diff_host_rate = 0.0
-        self.dst_host_s_error_rate       = 0.0
-        self.dst_host_srv_s_error_rate   = 0.0
-        self.dst_host_r_error_rate       = 0.0
-        self.dst_host_srv_r_error_rate   = 0.0
-        self.attack_type                 = ""
+    duration        = 0.0       #Length (in seconds) of connection
+    protocol        = ""        #Protocol name (tcp, udp)
+    service         = ""        #Service on destination (http, telnet)
+    flag            = ""        #normal or error status
+    src_bytes       = 0         #number of bytes from source to destination
+    dst_bytes       = 0         #number of bytes from destination to source
+    land            = False     #True if connection is to / from the same host / port
+    wrong_format    = 0         #number of "wrong" fragments
+    urgent          = 0         #number of urgent packets
+    hot             = 0         #number of "hot" indicators
+    failed_logins   = 0         #number of failed login attempts
+    logged_in       = False     #true if successfully loged in
+    compromised     = 0         #number of "compromised" conditions
+    root_shell      = False     #True if root shell was obtained
+    su_attempted    = False     #True if "su root" was attempted
+    roots           = 0         #number of "root" accesses
+    files_created   = 0         #number of file creation operations
+    shells          = 0         #number of shell prompts
+    access_files    = 0         #number of operations on access control files
+    outbound_cmds   = 0         #number of outbound commands in an ftp session
+    is_host_login   = False     #True if the login belongs to the host list
+    is_guest_login  = False     #True if the login is a guest login
+    conn_count      = 0         #number of connections to the same host in the last 2 seconds
 
-#------------------------------------------------------------------------------
-if __name__ == 'main':
-    print('log test')
+    #The following features refer to the same host connections
+    srv_count           = 0     # number of connections to the same service
+                                     #as the current connection in the past 2 secs
+    s_error_rate        = 0.0   # % of connections that have SYN errors
+    srv_s_error_rate    = 0.0   # % of connections that have SYN errors
+    r_error_rate        = 0.0   # % of connections that have REJ errors
+    srv_r_error_rate    = 0.0   # % of connections that have REJ errors
+    same_srv_rate       = 0.0   # % of the connections to the same service
+    diff_srv_rate       = 0.0   # % of connections to different services
+
+    #The following features refer to the same service connections
+    srv_diff_host_rate          = 0.0   # % of connections to different hosts
+    dst_host_count              = 0
+    dst_host_srv_count          = 0
+    dst_host_same_srv_rate      = 0.0
+    dst_host_diff_srv_rate      = 0.0
+    dst_host_same_src_port_rate = 0.0
+    dst_host_srv_diff_host_rate = 0.0
+    dst_host_s_error_rate       = 0.0
+    dst_host_srv_s_error_rate   = 0.0
+    dst_host_r_error_rate       = 0.0
+    dst_host_srv_r_error_rate   = 0.0
+    attack_type                 = ""
+
+    def __init__(self, row):
+        self.duration = float(row[0])
+        self.protocol = row[1]
+        self.service = row[2]
+        self.flag = row[3]
+        self.src_bytes = int(row[4])
+        self.dst_bytes = int(row[5])
+        self.land = bool(row[6])
+        self.wrong_format = int(row[7])
+        self.urgent = int(row[8])
+        self.hot = int(row[9])
+        self.failed_logins = int(row[10])
+        self.logged_in = bool(row[11])
+        self.compromised = int(row[12])
+        self.root_shell = bool(row[13])
+        self.su_attempted = bool(row[14])
+        self.roots = int(row[15])
+        self.files_created = int(row[16])
+        self.shells = int(row[17])
+        self.access_files = int(row[18])
+        self.outbound_cmds = int(row[19])
+        self.is_host_login = bool(row[20])
+        self.is_guest_login = bool(row[21])
+        self.conn_count = int(row[22])
+        self.srv_count = int(row[23])
+        self.s_error_rate = float(row[24])
+        self.srv_s_error_rate = float(row[25])
+        self.r_error_rate = float(row[26])
+        self.srv_r_error_rate = float(row[27])
+        self.same_srv_rate = float(row[28])
+        self.diff_srv_rate = float(row[29])
+        self.srv_diff_host_rate = float(row[30])
+        self.dst_host_count = int(row[31])
+        self.dst_host_srv_count = int(row[32])
+        self.dst_host_same_srv_rate = float(row[33])
+        self.dst_host_diff_srv_rate = float(row[34])
+        self.dst_host_same_src_port_rate = float(row[35])
+        self.dst_host_srv_diff_host_rate = float(row[36])
+        self.dst_host_s_error_rate = float(row[37])
+        self.dst_host_srv_s_error_rate = float(row[38])
+        self.dst_host_r_error_rate = float(row[39])
+        self.dst_host_srv_r_error_rate = float(row[40])
+        if len(row) >= 42:
+            self.attack_type = row[41] 
--- Main.py
+++ Main.py
@@ -6,5 +6,23 @@
 #Network Intrusion Project
 #------------------------------------------------------------------------------
 
+from csv import reader
+from Log import Log
+
+def parse_file(fname):
+    """Opens a file fname and creates a list of all 
+        the network intrusion logs"""
+    with open(fname, 'rb') as f:
+        dataFromFile = reader(f)
+        listOfData = []
+        for row in dataFromFile:
+            if row:
+                listOfData.append(Log(row))
+        return listOfData 
+
+def main():
+    parse_file("network_dataset.csv") 
+
+
 if __name__ == "__main__":
-    print("Now running...")
+    main()
