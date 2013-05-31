a89fa2f3d5e27ae4d7d4ea9a605a3239b34adb8f
Jonathan <hiafi3@gmail.com>
2013-05-30 23:27:49 -0700

    Added the dimensionality reduction

--- Util.py
+++ Util.py
@@ -8,6 +8,8 @@
 #Utility Functions
 
 #------------------------------------------------------------------------------
+from sklearn.decomposition import PCA
+
 def filter_by_type(data_set, class_name):
     return [obj for obj in data_set if obj.attack_type == class_name]
 
@@ -15,24 +17,24 @@ def normal_discrete_coordinates(data_set):
     final_data = []
     for attr in data_set[0].__dict__.iterkeys():
         if attr == "attack_type":
+            attack_data = [getattr(x, attr, 0) for x in data_set]
             continue
         data = [getattr(x, attr, 0) for x in data_set]
-        if (isinstance(getattr(data_set[0], attr, 0), float) or
-            isinstance(getattr(data_set[0], attr, 0), int)):    
-            mx, mn = (max(data), min(data))
-            if mx == 0:
-                mx, mn = (1, 0)
         for point_index, value in enumerate(data_set):
             if isinstance(data[point_index], str):
                 value = data_set[point_index].get_discrete_value(attr)
             elif isinstance(data[point_index], bool):
                 value = 1 if data[point_index] else 0
             else:
-                value = 1 if (data[point_index] - mn) / mx > .5 else 0
+                value = data[point_index]
+                #These are float values, this may need to be placed in buckets
 
             if len(final_data) <= point_index:
                 final_data.append( [value] )
             else:
                 final_data[point_index].append(value)
+    
+    pca = PCA(n_components=10, whiten=True).fit(final_data) #This may need to change
+    final_data = pca.transform(final_data) 
 
-    return final_data
+    return final_data, attack_data
