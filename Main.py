678d550b2cc17cece6f668d8040bec52896daca4
Nate <nnguyen939@gmail.com>
2013-05-31 12:07:03 -0700

    Added some progress information

--- Main.py
+++ Main.py
@@ -21,9 +21,14 @@ def parse_file(fname):
     with open(fname, 'rb') as f:
         dataFromFile = reader(f)
         listOfData = []
+        completed = 0
         for row in dataFromFile:
             if row:
                 listOfData.append(Log(row))
+                completed = completed + 1
+                sys.stdout.write("\r%d points loaded" %completed)
+                sys.stdout.flush()
+        print ""
         return listOfData 
 
 #------------------------------------------------------------------------------
@@ -33,24 +38,36 @@ def main():
         sys.exit()
 
     file_name = str(sys.argv[1])
+    print "Loading file..."
     data = parse_file(file_name)
 
     training_set, expected_set = normal_discrete_coordinates(data)
     trainer = Trainer(training_set, expected_set)
+    print "Training..."
     trainer.train()
 
     #Get our predicted set
-    predict_set = [trainer.predict(item) for item in training_set]
+    predict_set = []
+    completed = 0
+    print "Predicting..."
+    for item in training_set:
+        predict_set.append(trainer.predict(item))
+        completed = completed + 1
+        sys.stdout.write("\r%d points predicted" %completed)
+        sys.stdout.flush()
+
     
     correct = 0
+    completed = 0
+    print "\nComparing results..."
     for i in range(0, len(predict_set)):
+        completed = completed + 1
+        sys.stdout.write("\r%d points predicted" %completed)
+        sys.stdout.flush()
         if predict_set[i] == expected_set[i]:
             correct = correct + 1
-            print str(predict_set[i]) + " == " + str(expected_set[i])
-        else: 
-            print str(predict_set[i]) + " != " + str(expected_set[i])
 
-    print "Prediction rate: " 
+    print "\nPrediction rate: " 
     print str(correct / len(predict_set) * 100 ) + "%"
         
 
