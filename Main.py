46188029a5a6c2f6b121df2f66abb5443f3ec2f5
Nate <nnguyen939@gmail.com>
2013-05-31 11:48:13 -0700

    Can now predict and learn from trainign set

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ from csv import reader
 from Log import Log
 from Log import attack_types
 from Analysis import attribute_clustering, average_value, Trainer
-from Util import filter_by_type
+from Util import filter_by_type, normal_discrete_coordinates
 import sys
 
 
@@ -35,32 +35,23 @@ def main():
     file_name = str(sys.argv[1])
     data = parse_file(file_name)
 
-    training_set = [] #A list of all coordinates from data as a list 
-    expected_set = [] #A list of all expected results  of training_set
+    training_set, expected_set = normal_discrete_coordinates(data)
     trainer = Trainer(training_set, expected_set)
     trainer.train()
 
-    testing_set = [] #A list of all coordinates as a list we're going to predict
-    for item in testing_set:
-        print trainer.predict(item) # note return value from predict is an int
+    #Get our predicted set
+    predict_set = [trainer.predict(item) for item in training_set]
+    
+    correct = 0
+    for i in range(0, len(predict_set)):
+        if predict_set[i] == expected_set[i]:
+            correct = correct + 1
+            print str(predict_set[i]) + " == " + str(expected_set[i])
+        else: 
+            print str(predict_set[i]) + " != " + str(expected_set[i])
 
-
-
-    for attrName, attrValue in data[0].__dict__.iteritems():
-        if isinstance(attrValue, bool):
-            continue
-        if isinstance(attrValue, str):
-            continue
-
-        print attrName
-        for attack_type in attack_types:
-            type_data = filter_by_type(data, attack_type)
-            if len(type_data) > 0:
-                print "{0} - Avg Distance (normal): {1}, Average: {2}".format(
-                attack_type, 
-                attribute_clustering(type_data, attrName, True),
-                average_value(type_data, attrName, False))
-        print ""
+    print "Prediction rate: " 
+    print str(correct / len(predict_set) * 100 ) + "%"
         
 
 #------------------------------------------------------------------------------
